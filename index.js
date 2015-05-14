8b1b96993ff2966155a4c3d74cc5f517e35770d6
Oliver Salzburg <oliver.salzburg@gmail.com>
2015-05-14 16:11:35 +0200

    [TASK] Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = tryRequire;
+
+function tryRequire( moduleReference ) {
+  var module = null;
+  try {
+    module = require( moduleReference );
+  
+  } catch( error ) {
+    this.lastError = error;
+  }
+  
+  return module;
+}
