a47d4ab431c382260e8b1cb6a93d3138442ba156
Oliver Salzburg <oliver.salzburg@gmail.com>
2015-05-21 10:34:32 +0200

    [TASK] Fix formatting

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 module.exports = tryRequire;
 
-function tryRequire( moduleReference ) {
-  var module = null;
-  try {
-    module = require( moduleReference );
-  
-  } catch( error ) {
-    this.lastError = error;
-  }
-  
-  return module;
+function tryRequire(moduleReference) {
+	var module = null;
+	try {
+		module = require(moduleReference);
+
+	} catch(error) {
+		this.lastError = error;
+	}
+
+	return module;
 }
